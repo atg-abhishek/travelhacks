3a6703d06a3b212bf15977af063d863f1039d23d
atg.abhishek <atg.abhishek@gmail.com>
2016-06-18 15:44:57 -0400

    asdad

--- backend/server.py
+++ backend/server.py
@@ -7,5 +7,10 @@ app = Flask(__name__)
 def landing():
 	return "Welcome to the Travel hackathon team"
 
+@app.route('/hello')
+def hello():
+	return "Hello Clemence"
+
+
 if __name__ == '__main__':
 	app.run(host='0.0.0.0', port=23001, debug=True)
