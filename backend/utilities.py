822e3a50070d4a42f5f6fc71a03cccd1b4a3f02e
atg.abhishek <atg.abhishek@gmail.com>
2016-06-19 10:18:58 -0400

    fixed garbage

--- backend/utilities.py
+++ backend/utilities.py
@@ -288,8 +288,6 @@ def generate_itinerary(city, moods):
             return 'null'
         morning_activity, afternoon_activity, nightlife = random.sample(mood_arrs[moods[0]], 3)
     elif len(moods) >= 2:
-        pprint("moods 0 is " + moods[0] + " and " + moods[1])
-        pprint(" mood arrs list is " + mood_arrs[moods[0]])
         morning_activity = random.choice(mood_arrs[moods[0]])
         afternoon_activity = random.choice(mood_arrs[moods[1]])
         nightlife = random.choice(mood_arrs[moods[1]])
