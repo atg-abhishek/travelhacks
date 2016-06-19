87776703e8bb92d6268497d4dc81f0f87b9a9e1a
atg.abhishek <atg.abhishek@gmail.com>
2016-06-18 21:10:51 -0400

    moved weather code

--- backend/utilities.py
+++ backend/utilities.py
@@ -170,6 +170,33 @@ def get_city_from_lat_lng(lat=45.5268224, lng=-73.5799845):
     r = requests.get(url)
     return r.json()['address']['city']
 
+def get_weather(lat=45.5268224, lng=-73.5799845):
+    api_key = key_fetcher['weather_key']
+    url1 = "http://api.openweathermap.org/data/2.5/weather?lat="+str(lat)+"&lon="+str(lng)+"&APPID="+api_key
+    r = requests.get(url1)
+    print(r.text)
+    r = r.json()
+    weather_type = r['weather'][0]['main']
+    supp_weather_info = r['weather'][0]['description']
+    w_info = weather_type + " " + supp_weather_info
+    w_dec = False
+    if re.search('rain', w_info, re.IGNORECASE) or re.search('cloud', w_info, re.IGNORECASE) or re.search('snow', w_info, re.IGNORECASE):
+        w_dec = True
+    humidity = r['main']['humidity']
+    hum_dec = False
+    if humidity >=70:
+        hum_dec = True
+    max_temp = r['main']['temp_max']
+    min_temp = r['main']['temp_min']
+    temp = r['main']['temp'] - 273.15
+    weather_decision = hum_dec and w_dec
+    wdecstr = ""
+    if weather_decision:
+        wdecstr="inside"
+    else:
+        wdecstr="outside"
+    return {'temp' : temp, 'type_of_activities' : wdecstr}
+
 # pprint(get_city_from_lat_lng())
 # pprint(get_list_of_activities("newyork", categories=["Adventures", "Spa"]))
 
